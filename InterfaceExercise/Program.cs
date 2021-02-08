3d68c2feedea7c8c8f485acade0de3df6465ee33
carlbruck <carl.bruckner@gmail.com>
2021-02-08 17:18:26 -0500

    Rough Draft

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterfaceExercise
 {
@@ -6,13 +7,29 @@ namespace InterfaceExercise
     {
         static void Main(string[] args)
         {
-            //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
+            var car = new Car();
 
-            //Create 2 Interfaces called IVehicle & ICompany
+            var truck = new Truck();
+
+            var vehicles = new List<IVehicle>() { car, truck };
+
+
+            foreach(var vehicle in vehicles)
+            {
+                vehicle.Drive();
+                vehicle.ChangedGears(true);
+                vehicle.Reverse();
+
+            }
+
+
+            //DONE - TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
+
+            //DONE - Create 2 Interfaces called IVehicle & ICompany
 
             //Create 3 classes called Car , Truck , & SUV
 
-            //In your IVehicle
+            //DONE - In your IVehicle
             
                 /* Create 4 members that Car, Truck, & SUV all have in common.
                  * Example: All vehicles have a number of wheels... for now..
---
+++ InterfaceExercise/Truck.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class Truck : IVehicle, ICompany
+    {
+        public Truck()
+        {
+
+        }
+        public double EngineSize { get; set; } = 6;
+        public string Make { get; set; } = "Ford";
+        public string Model { get; set; } = "F-150";
+        public int SetCount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Ford";
+        public string Motto { get; set; } = "Built Ford Tough";
+
+        public bool HasFourWheelDrive { get; set; } = true;
+
+        public bool HasChangedGears { get; set; }
+
+
+        
+
+       
+        public int SeatCount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward");
+        }
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we change gears!");
+            }
+
+        }
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now in park");
+                HasChangedGears = false;
+                }
+            else
+            {
+                Console.WriteLine("Can't park until we change gears!");
+            }
+        }
+
+        public void ChangedGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+
+        //bool IVehicle.ChangedGears(bool isChanged)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+}
+
+
+
+
+
+
